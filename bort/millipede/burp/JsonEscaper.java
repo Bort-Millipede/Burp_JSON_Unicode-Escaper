c35f535488fc202b28a5c9cc3928ff40d1c83dbf
Jeffrey Cap <jeffrey.m.cap@gmail.com>
2024-10-10 21:39:57 -0500

    Partial implementation of Settings tab (global settings)

--- bort/millipede/burp/JsonEscaper.java
+++ bort/millipede/burp/JsonEscaper.java
@@ -5,6 +5,7 @@ import burp.api.montoya.extension.Extension;
 import burp.api.montoya.intruder.*;
 import burp.api.montoya.ui.*;
 import burp.api.montoya.ui.contextmenu.*;
+import burp.api.montoya.persistence.*;
 import burp.api.montoya.core.*;
 import burp.api.montoya.http.message.*;
 import burp.api.montoya.http.message.requests.*;
@@ -31,13 +32,16 @@ public class JsonEscaper implements BurpExtension,ContextMenuItemsProvider {
 	private Intruder bIntruder;
 	private UserInterface bUI;
 	
-	static Logging mLogging;
+	private static Preferences bPreferences;
+	private static Logging mLogging;
 	
 	private JMenuItem unescapeMenuItem;
 	private JMenuItem escapeKeyMenuItem;
 	private JMenuItem unicodeEscapeKeyMenuItem;
 	private JMenuItem unicodeEscapeAllMenuItem;
 	private JMenuItem unicodeEscapeMenuItem;
+	
+	private JsonEscaperTab escaperTab;
 	
 	public static final String EXTENSION_NAME = "JSON Unicode-Escaper";
 	public static final String EXTENSION_VERSION = "0.1";
@@ -47,6 +51,10 @@ public class JsonEscaper implements BurpExtension,ContextMenuItemsProvider {
 	public static final String UNICODE_ESCAPE_ALL_LABEL = "JSON Unicode-escape all chars";
 	public static final String UNICODE_ESCAPE_CUSTOM_LABEL = "JSON Unicode-escape custom chars";
 	public static final String INLINE_JSON_KEY = "input";
+	public static final String KEY_CHARS = "\000\001\002\003\004\005\006\007\010\011\012\013\014\015\016\017\020\021\022\023\024\025\026\027\030\031\032\033\034\035\036\037\"\\";
+	public static final String CHARS_TO_ESCAPE_KEY = "JsonEscaper.charsToEscape";
+	public static final String INCLUDE_KEY_CHARS_KEY = "JsonEscaper.includeKeyChars";
+	public static final String FINE_TUNE_UNESCAPING_KEY = "JsonEscaper.fineTuneUnescape";
 	
 	@Override
 	public void initialize(MontoyaApi api) {
@@ -69,17 +77,25 @@ public class JsonEscaper implements BurpExtension,ContextMenuItemsProvider {
 		unicodeEscapeAllMenuItem = new JMenuItem(UNICODE_ESCAPE_ALL_LABEL);
 		unicodeEscapeMenuItem = new JMenuItem(UNICODE_ESCAPE_CUSTOM_LABEL+" [NOT FULLY IMPLEMENTED]");
 		
-		JsonEscaperTab escaperTab = new JsonEscaperTab(mApi);
+		escaperTab = new JsonEscaperTab(mApi);
 		bUI.applyThemeToComponent(escaperTab);
 		bUI.registerSuiteTab(EXTENSION_NAME,escaperTab);
 		
+		bPreferences = mApi.persistence().preferences();
 		mLogging = mApi.logging();
+		mLogging.logToOutput(String.format("%s v%s initialized.",EXTENSION_NAME,EXTENSION_VERSION));
 	}
 	
 	@Override
 	public List<Component> provideMenuItems(ContextMenuEvent event) {
 
-		if(event.isFrom(InvocationType.INTRUDER_PAYLOAD_POSITIONS,InvocationType.MESSAGE_EDITOR_REQUEST,InvocationType.MESSAGE_EDITOR_RESPONSE,InvocationType.MESSAGE_VIEWER_REQUEST,InvocationType.MESSAGE_VIEWER_RESPONSE)) {
+		if(event.isFrom(
+		InvocationType.INTRUDER_PAYLOAD_POSITIONS,
+		InvocationType.MESSAGE_EDITOR_REQUEST,
+		InvocationType.MESSAGE_EDITOR_RESPONSE,
+		InvocationType.MESSAGE_VIEWER_REQUEST,
+		InvocationType.MESSAGE_VIEWER_RESPONSE
+		)) {
 			//Re-enable menu items
 			if(!unescapeMenuItem.isEnabled()) unescapeMenuItem.setEnabled(true);
 			if(!escapeKeyMenuItem.isEnabled()) escapeKeyMenuItem.setEnabled(true);
@@ -143,47 +159,50 @@ public class JsonEscaper implements BurpExtension,ContextMenuItemsProvider {
 	public static String unescapeAllChars(String input) throws JSONException {
 		if(input==null) return null;
 		if(input.length()==0) return input;
-		if(!input.contains("\\")) return input;
+		if(!input.contains("\\")) return input; //do not process input not containing \ characters (implying no escaping in input)
 		
+		//If enabled in Settings menu: Attempt to prevent errors when processing text that is not actually escaped.
 		String sanitizedInput = input;
-		if(sanitizedInput.contains("\n")) sanitizedInput = sanitizedInput.replace("\n","\\u000a"); //escape raw newline characters if present
-		if(sanitizedInput.contains("\"")) { //" characters in string to potentially unescape: properly escape " and \ characters for inline JSON if necessary
-			int i=sanitizedInput.length()-1;
-			while(i>=0) {
-				if(sanitizedInput.charAt(i)=='\"') { //" character found
-					int end = i;
-					int backslashCount = 0;
-					if(i>0) { //count \ characters preceding "
-						i--;
-						char prev = sanitizedInput.charAt(i);
-						while(i>=0 && prev=='\\') {
-							backslashCount++;
+		if(bPreferences.getBoolean(FINE_TUNE_UNESCAPING_KEY)) {
+			if(sanitizedInput.contains("\n")) sanitizedInput = sanitizedInput.replace("\n","\\u000a"); //escape raw newline characters if present
+			if(sanitizedInput.contains("\"")) { //" characters in string to potentially unescape: properly escape " and \ characters for inline JSON if necessary
+				int i=sanitizedInput.length()-1;
+				while(i>=0) {
+					if(sanitizedInput.charAt(i)=='\"') { //" character found
+						int end = i;
+						int backslashCount = 0;
+						if(i>0) { //count \ characters preceding "
 							i--;
-							prev = sanitizedInput.charAt(i);
+							char prev = sanitizedInput.charAt(i);
+							while(i>=0 && prev=='\\') {
+								backslashCount++;
+								i--;
+								prev = sanitizedInput.charAt(i);
+							}
+							i++;
 						}
-						i++;
+						
+						String quoteBackslashReplace = "";
+						int j=0;
+						while(j<(backslashCount/2)) { //replace escaped \ characters with unicode-escaped \ characters
+							quoteBackslashReplace = quoteBackslashReplace.concat("\\u005c");
+							j++;
+						}
+						quoteBackslashReplace = quoteBackslashReplace.concat("\\u0022"); //unicode-escape " character
+						
+						String prefix = sanitizedInput.substring(0,i);
+						String suffix = "";
+						if(sanitizedInput.length()!=end+1) {
+							suffix = sanitizedInput.substring(end+1,sanitizedInput.length());
+						}
+						sanitizedInput = prefix.concat(quoteBackslashReplace).concat(suffix); //replace original " and \ characters discovered above with unicode-escape characters
 					}
-					
-					String quoteBackslashReplace = "";
-					int j=0;
-					while(j<(backslashCount/2)) { //replace escaped \ characters with unicode-escaped \ characters
-						quoteBackslashReplace = quoteBackslashReplace.concat("\\u005c");
-						j++;
-					}
-					quoteBackslashReplace = quoteBackslashReplace.concat("\\u0022"); //unicode-escape " character
-					
-					String prefix = sanitizedInput.substring(0,i);
-					String suffix = "";
-					if(sanitizedInput.length()!=end+1) {
-						suffix = sanitizedInput.substring(end+1,sanitizedInput.length());
-					}
-					sanitizedInput = prefix.concat(quoteBackslashReplace).concat(suffix); //replace original " and \ characters discovered above with unicode-escape characters
+					i--;
 				}
-				i--;
+				
+				//todo: add timestamps to logs?
+				mLogging.logToOutput("sanitizedInput: "+sanitizedInput);
 			}
-			
-			//todo: add timestamps to logs?
-			mLogging.logToOutput("sanitizedInput: "+sanitizedInput);
 		}
 
 		JSONObject jsonObj = null;
--- bort/millipede/burp/ui/EscaperSettingsTab.java
+++ bort/millipede/burp/ui/EscaperSettingsTab.java
@@ -1,34 +1,119 @@
 package bort.millipede.burp.ui;
 
+import bort.millipede.burp.JsonEscaper;
+
 import burp.api.montoya.MontoyaApi;
 import burp.api.montoya.core.ByteArray;
 import burp.api.montoya.ui.editor.RawEditor;
 import burp.api.montoya.ui.editor.EditorOptions;
+import burp.api.montoya.persistence.Preferences;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.DataFlavor;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.GridLayout;
+import java.awt.Color;
 import javax.swing.*;
 
-class EscaperSettingsTab extends JPanel {
+class EscaperSettingsTab extends JPanel implements ActionListener {
 	private MontoyaApi mApi;
+	private Preferences preferences;
+	
+	private JTextArea charsToEscapeField;
+	private JButton pasteButton;
+	private JButton deduplicateButton;
+	private JCheckBox includeKeyCharsCheckbox;
+	
+	private JCheckBox fineTuneUnescapingCheckbox;
 	
 	EscaperSettingsTab(MontoyaApi api) {
 		super();
 		mApi = api;
+		preferences = mApi.persistence().preferences();
 		
-		//setup "Settings" Tab
-		JPanel innerPanel = new JPanel(new GridLayout(3,2));
-		innerPanel.add(new JLabel("Characters to JSON Unicode-escape:",SwingConstants.RIGHT));
-		//innerPanel.add(new JTextField());
-		innerPanel.add(new JScrollPane(mApi.userInterface().createRawEditor(EditorOptions.SHOW_NON_PRINTABLE_CHARACTERS,EditorOptions.WRAP_LINES).uiComponent()));
-		innerPanel.add(new JLabel());
-		JPanel buttonPanel = new JPanel(new GridLayout(2,1));
-		buttonPanel.add(new JButton("Paste"));
-		buttonPanel.add(new JButton("Deduplicate characters"));
-		innerPanel.add(buttonPanel);
-		innerPanel.add(new JLabel("Automatically include JSON key characters:",SwingConstants.RIGHT));
-		JCheckBox includeKeyChars = new JCheckBox();
-		innerPanel.add(includeKeyChars);
+		JPanel innerPanel = new JPanel();
+		innerPanel.setLayout(new BoxLayout(innerPanel, BoxLayout.Y_AXIS));
+		
+		//"escape custom chars" settings panel
+		innerPanel.add(new JLabel(String.format("\"%s\" Settings",JsonEscaper.UNICODE_ESCAPE_CUSTOM_LABEL)));
+		JPanel innerSettingsPanel = new JPanel(new GridLayout(3,2));
+		innerSettingsPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+		innerSettingsPanel.add(new JLabel("Characters to JSON Unicode-escape:",SwingConstants.RIGHT));
+		charsToEscapeField = new JTextArea("");
+		charsToEscapeField.setLineWrap(true);
+		innerSettingsPanel.add(new JScrollPane(charsToEscapeField));
+		innerSettingsPanel.add(new JLabel());
+		JPanel buttonPanel = new JPanel(new GridLayout(1,2));
+		pasteButton = new JButton("Paste");
+		pasteButton.addActionListener(this);
+		buttonPanel.add(pasteButton);
+		deduplicateButton = new JButton("Deduplicate characters");
+		deduplicateButton.addActionListener(this);
+		buttonPanel.add(deduplicateButton);
+		innerSettingsPanel.add(buttonPanel);
+		innerSettingsPanel.add(new JLabel("Automatically include JSON key characters:",SwingConstants.RIGHT));
+		includeKeyCharsCheckbox = new JCheckBox("",true);
+		includeKeyCharsCheckbox.addActionListener(this);
+		innerSettingsPanel.add(includeKeyCharsCheckbox);
+		preferences.setBoolean(JsonEscaper.INCLUDE_KEY_CHARS_KEY,true);
+		innerPanel.add(innerSettingsPanel);
+		
+		//separators
+		innerPanel.add(new JLabel(" "));
+		innerPanel.add(new JLabel(" "));
+		
+		//global settings panel
+		innerPanel.add(new JLabel("Global Settings"));
+		JPanel innerGlobalPanel = new JPanel(new GridLayout(1,2));
+		innerGlobalPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+		innerGlobalPanel.add(new JLabel("Fine-tune unescaping to avoid errors:",SwingConstants.RIGHT));
+		fineTuneUnescapingCheckbox = new JCheckBox("",true);
+		fineTuneUnescapingCheckbox.addActionListener(this);
+		innerGlobalPanel.add(fineTuneUnescapingCheckbox);
+		preferences.setBoolean(JsonEscaper.FINE_TUNE_UNESCAPING_KEY,true);
+		innerPanel.add(innerGlobalPanel);
 		
 		this.add(innerPanel);
+	}
+	
+	//ActionListener method
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		if(source == pasteButton) {
+			Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+			Transferable t = cb.getContents(null);
+			try {
+				charsToEscapeField.setText(charsToEscapeField.getText()+(String) t.getTransferData(DataFlavor.stringFlavor));
+			} catch(Exception ex) {
+				//No data in clipboard, or data is not text: do nothing
+			}
+		} else if(source == deduplicateButton) {
+			deduplicateEscapeChars();
+		} else if(source == includeKeyCharsCheckbox) {
+			preferences.setBoolean(JsonEscaper.INCLUDE_KEY_CHARS_KEY,includeKeyCharsCheckbox.isSelected());
+		} else if(source == fineTuneUnescapingCheckbox) {
+			preferences.setBoolean(JsonEscaper.FINE_TUNE_UNESCAPING_KEY,fineTuneUnescapingCheckbox.isSelected());
+		}
+	}
+	
+	private void deduplicateEscapeChars() {
+		String charsToEscape = charsToEscapeField.getText();
+		
+		//Remove duplicate characters
+		if(charsToEscape!=null && charsToEscape.length()!=0) {
+			String uniqEscapeChars = "";
+			for(int l=0;l<charsToEscape.length();l++) {
+				String ch = String.valueOf(charsToEscape.charAt(l));
+				if(!uniqEscapeChars.contains(ch))
+					uniqEscapeChars += ch;
+			}
+			charsToEscapeField.setText(uniqEscapeChars);
+			preferences.setString(JsonEscaper.CHARS_TO_ESCAPE_KEY,uniqEscapeChars);
+		}
 	}
 }
